5f9144c007eabcb9f747554b356b151acba66e2b
Jasmine <jasmineqw45@gmail.com>
2023-09-02 03:36:45 -0700

    overhauled color scheme and hero page bc i didnt vibe w it - testing out something new

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ const toggleButton = document.querySelector('.toggle-button')
                 ? 'fa-solid fa-xmark'
                 : 'fa-solid fa-bars'
             }
+
+
+// toggle hover icons
+
+const hoverIcons = document.querySelector('.icons-contain');
+const iconText = document.querySelector('.icons-contain div p');
+const iconMain = document.querySelector('.icons-contain div i');
+
+// Initially, hide iconText
+iconText.style.display = 'none';
+
+// Add an event listener for mouseenter
+iconMain.addEventListener('mouseenter', () => {
+    // Show iconText on mouseenter
+    iconText.style.display = 'contents';
+});
+
+// Add an event listener for mouseleave
+iconMain.addEventListener('mouseleave', () => {
+    // Hide iconText on mouseleave
+    iconText.style.display = 'none';
+});
